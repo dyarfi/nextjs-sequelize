8984a2f2a64a5531e21d3e7f8ec73196791087f4
zomars <zomars@me.com>
2021-02-10 13:38:23 -0700

    Uses mysql for development

--- db/config/config.js
+++ db/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'sqlite',
+    dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USER,
