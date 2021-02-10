fa2dbfaf416358e85b32825eee809392ba798aa0
zomars <zomars@me.com>
2021-02-10 12:29:20 -0700

    Fixes posts seeder

--- db/seeders/20200603082530-posts.js
+++ db/seeders/20200603082530-posts.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [
+    return queryInterface.bulkInsert('posts', [
       {
         userId: 1,
         title: 'Title post one',
